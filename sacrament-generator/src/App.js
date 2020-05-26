f6a54c2e6fcc845993e38795d7d1f0a385cc4720
Logan Hufstetler <logan.hufstetler@gmail.com>
2020-05-26 15:31:26 -0600

    started Landing component

--- sacrament-generator/src/App.js
+++ sacrament-generator/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
 
+import Landing from "./landing/Landing.js";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <Landing />
     </div>
   );
 }
