00574fd6e163f86480511b7652db5980d1868b31
Logan Hufstetler <logan.hufstetler@gmail.com>
2020-06-17 12:40:34 -0600

    added Login form Logic. Still need to add form validation.

--- sacrament-generator/src/components/Login/Login.js
+++ sacrament-generator/src/components/Login/Login.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Login() {
+    const [user, setUser] = useState({ username: "", password: "" });
+
+    const handleChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(`Username: ${user.username}\nPassword: ${user.password}`);
+        setUser({ username: "", password: "" });
+    };
+
     return (
-        <div>
-            <h3>Login</h3>
+        <div className="Login">
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <label>
+                    Username:
+                    <input
+                        type="text"
+                        name="username"
+                        value={user.username}
+                        onChange={(e) => handleChange(e)}
+                    />
+                </label>
+                <label>
+                    Password:
+                    <input
+                        type="text"
+                        name="password"
+                        value={user.password}
+                        onChange={(e) => handleChange(e)}
+                    />
+                </label>
+                <label>
+                    <button>Login</button>
+                </label>
+            </form>
         </div>
     );
 }
